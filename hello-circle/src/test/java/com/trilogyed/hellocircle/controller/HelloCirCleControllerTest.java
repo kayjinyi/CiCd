1b2831ba838d2e37a15960d90f3856a61e417fce
kayjinyi <95940771+kayjinyi@users.noreply.github.com>
2022-11-02 13:36:38 -0700

    fixing

--- hello-circle/src/test/java/com/trilogyed/hellocircle/controller/HelloCirCleControllerTest.java
+++ hello-circle/src/test/java/com/trilogyed/hellocircle/controller/HelloCirCleControllerTest.java
@@ -3,6 +3,7 @@ package com.trilogyed.hellocircle.controller;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-
+@WebMvcTest(HelloCirCleController.class)
 public class HelloCirCleControllerTest {
     @Autowired
     private MockMvc mockMvc;
